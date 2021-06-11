5039ca47fc273099f4f193ac1ff5ddaa1d308e52
AlekseyDolinin <allDolinin@gmail.com>
2021-06-11 09:41:14 +0300

    2.3

--- AppGiff/Controllers/Start/AppTracking.swift
+++ AppGiff/Controllers/Start/AppTracking.swift
@@ -1,11 +1,12 @@
 import UIKit
-//import AppTrackingTransparency
+#if canImport(AppTrackingTransparency)
+import AppTrackingTransparency
 
 extension StartViewController {
     
     func showModalAppTrackingDescription() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            guard #available(iOS 14, *) else { return }
+            guard #available(iOS 14.5, *) else { return }
             let vc = self.storyboard?.instantiateViewController(withIdentifier: "AppTrackingModalViewController") as! AppTrackingModalViewController
             vc.modalPresentationStyle = .fullScreen
             self.present(vc, animated: true)
@@ -20,29 +21,29 @@ extension StartViewController {
         print("requestTrackingAuthorization")
         #endif
         guard #available(iOS 14.5, *) else { return }
-//        ATTrackingManager.requestTrackingAuthorization { status in
-//            switch status {
-//            case .notDetermined:
-//                print("notDetermined")
-//                statusAppTracking = "notDetermined"
-//                break
-//            case .restricted:
-//                print("restricted")
-//                statusAppTracking = "restricted"
-//                break
-//            case .denied:
-//                print("denied")
-//                statusAppTracking = "denied"
-//                break
-//            case .authorized:
-//                print("authorized")
-//                statusAppTracking = "authorized"
-//                break
-//            @unknown default:
-//                break
-//            }
-//            UserDefaults.standard.setValue(statusAppTracking, forKey: "statusATTKey")
-//        }
+        ATTrackingManager.requestTrackingAuthorization { status in
+            switch status {
+            case .notDetermined:
+                print("notDetermined")
+                statusAppTracking = "notDetermined"
+                break
+            case .restricted:
+                print("restricted")
+                statusAppTracking = "restricted"
+                break
+            case .denied:
+                print("denied")
+                statusAppTracking = "denied"
+                break
+            case .authorized:
+                print("authorized")
+                statusAppTracking = "authorized"
+                break
+            @unknown default:
+                break
+            }
+            UserDefaults.standard.setValue(statusAppTracking, forKey: "statusATTKey")
+        }
     }
-    
 }
+#endif
